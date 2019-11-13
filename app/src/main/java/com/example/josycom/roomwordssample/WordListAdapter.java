829785f4183a3efe4eef452693e7d4b5df147c59
MayorJay <joseolu4gsm@yahoo.com>
2019-11-13 22:12:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/josycom/roomwordssample/WordListAdapter.java
@@ -0,0 +1,61 @@
+package com.example.josycom.roomwordssample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class WordListAdapter extends RecyclerView.Adapter<WordListAdapter.WordViewHolder> {
+    private LayoutInflater mInflater;
+    private List<Word> mWords;
+
+    WordListAdapter(Context context){
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @NonNull
+    @Override
+    public WordViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = mInflater.inflate(R.layout.recyclerview_item, parent, false);
+        return new WordViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull WordViewHolder holder, int position) {
+        if (mWords != null){
+            Word current = mWords.get(position);
+            holder.wordItemView.setText(current.getWord());
+        } else {
+            holder.wordItemView.setText(R.string.no_word);
+        }
+    }
+
+    void setWords(List<Word> words){
+        mWords = words;
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mWords != null){
+            return mWords.size();
+        } else {
+            return 0;
+        }
+    }
+
+    class WordViewHolder extends RecyclerView.ViewHolder{
+        private final TextView wordItemView;
+
+        private WordViewHolder(@NonNull View itemView) {
+            super(itemView);
+            wordItemView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
